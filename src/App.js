a4512f5bbf8a880efad750073d108c87a6e0d203
ConstantineF <warsawxx@gmail.com>
2023-02-18 00:35:07 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  // api: https://goweather.herokuapp.com/weather/Curitiba
+  // const key = process.env.REACT_APP_API_KEY;
+  // const funWeather = (place) => {
+  //   fetch(`https://goweather.herokuapp.com/weather/${place}`)
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // };
+
+  useEffect(() => {
+    /*
+    const city = prompt("Provide city");
+    const funWeather = (place) => {
+      fetch(`https://goweather.herokuapp.com/weather/${place}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    };
+    funWeather(city);
+    */
+
+    navigator.geolocation.getCurrentPosition((position) =>
+      console.log(position)
+    );
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src="" className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
