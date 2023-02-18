5bf0cdf8aded04c8afb437ffe1eb2d373b31ae07
ConstantineF <warsawxx@gmail.com>
2023-02-19 02:20:02 +0400

    .

--- src/App.js
+++ src/App.js
@@ -1,38 +1,99 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  // api: https://goweather.herokuapp.com/weather/Curitiba
-  // const key = process.env.REACT_APP_API_KEY;
-  // const funWeather = (place) => {
-  //   fetch(`https://goweather.herokuapp.com/weather/${place}`)
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data));
-  // };
+  const [myCity, setMyCity] = useState();
+  const [cityFound, setCityFound] = useState(false);
+  const [weather, setWeather] = useState();
+  // weather api: https://goweather.herokuapp.com/weather/Curitiba
+  // location api: 'https://geocode.xyz/51.50354,-0.12768?geoit=xml&auth=your_api_key' || https://geocode.xyz/${lat},${lng}?geoit=json
+
+  const getLocationAndWeather = async () => {
+    await navigator.geolocation.getCurrentPosition(
+      async ({ coords: { latitude, longitude } }) => {
+        try {
+          const res = await fetch(
+            `https://geocode.xyz/${latitude},${longitude}?geoit=json`
+          );
+
+          if (!res.ok) {
+            throw new Error(`Error: ${res.status}`);
+          } else {
+            const { city } = await res.json();
+            setCityFound(true);
+            setMyCity(city);
+
+            const weatherAPI = await fetch(
+              `https://goweather.herokuapp.com/weather/${city}`
+            );
+            const weatherData = await weatherAPI.json();
+            setWeather(weatherData);
+            console.log(weatherData);
+          }
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+    );
+  };
 
   useEffect(() => {
-    /*
-    const city = prompt("Provide city");
-    const funWeather = (place) => {
-      fetch(`https://goweather.herokuapp.com/weather/${place}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data));
-    };
-    funWeather(city);
-    */
-
-    navigator.geolocation.getCurrentPosition((position) =>
-      console.log(position)
-    );
+    getLocationAndWeather();
   }, []);
+
+  //   {
+  //     "temperature": "+3 °C",
+  //     "wind": "41 km/h",
+  //     "description": "Partly cloudy",
+  //     "forecast": [
+  //         {
+  //             "day": "1",
+  //             "temperature": "5 °C",
+  //             "wind": "9 km/h"
+  //         },
+  //         {
+  //             "day": "2",
+  //             "temperature": "+7 °C",
+  //             "wind": "20 km/h"
+  //         },
+  //         {
+  //             "day": "3",
+  //             "temperature": "+9 °C",
+  //             "wind": "11 km/h"
+  //         }
+  //     ]
+  // }
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src="" className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <h1>Current Weather</h1> */}
+        {myCity ? (
+          <h2>Weather in {myCity}</h2>
+        ) : (
+          <h2>City not found. Try again.</h2>
+        )}
+        {myCity ? (
+          <div>
+            <p>{myCity}</p>
+            <p>Temperature: {weather.temperature}</p>
+            <p>Weather: {weather.description}</p>
+            <p>Wind: {weather.wind}</p>
+
+            <ul>
+              {weather.forecast.map((day) => (
+                <li>{day.temperature}</li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Try once again</p>
+        )}
+        {/* {cityFound ? <p>City found</p> : <p>City not found yet</p>} */}
+
+        <p></p>
+        <button onClick={getLocationAndWeather}>Try again</button>
       </header>
     </div>
   );
